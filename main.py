f7a4b54586c04201866ccb7f4abf51c937a03ba4
LordRingono <40274235+LordRingono@users.noreply.github.com>
2024-03-12 18:56:01 +0000

    La bdd est connecte

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, request, url_for, redirect, session
-
+import os
 app = Flask(__name__)
 
-
+import pymongo
+mongo = pymongo.MongoClient(os.getenv("MONGO_KEY"))
 
 @app.route('/')
 def index():
- # db_manganimes = mongo.db.animes
-# manganimes = db_manganimes.find({})
+  db_animes = mongo.db.animes
+  animes = db_animes.find({})
+  db_mangas = mongo.db.mangas
+  mangas = db_mangas.find({})
   # variable qui servira a contenir les infos de la bdd
   # titre du manganime : titre
   # image du manganime : url_image
   # lien du manganime : url_manganime
   # id du manganime : _id
-  return render_template("index.html")
-  #return render_template("index.html", manganimes=manganimes)
+  # return render_template("index.html")
+  return render_template("index.html", animes=animes, mangas=mangas)
 
 @app.route('/mangas')
 def mangas():
@@ -24,9 +27,16 @@ def mangas():
 def animes():
   return render_template("animes.html")
 
-@app.route('/manganime/<id_manganime>')
-def manganime():
-
+@app.route('/manganime/<type>/<id_manganime>')
+def manganime(type, id_manganime):
+  if type == "anime" :
+    db_animes = mongo.db.animes
+    anime = db_animes.find_one({"_id": id_manganime})
+    return render_template("manganime.html", anime=anime)
+  elif type == "manga" :
+    db_mangas = mongo.db.mangas
+    manga = db_mangas.find_one({"_id": id_manganime})
+    return render_template("manganime.html", manga=manga)
   return render_template("manganime.html")
 
 @app.route('/login')
@@ -44,6 +54,10 @@ def add():
 @app.route('/admin')
 def admin():
   return render_template("admin.html")
+
+@app.route('/logout')
+def logout():
+  return render_template("logout.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
